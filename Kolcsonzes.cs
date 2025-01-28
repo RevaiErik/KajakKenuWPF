f0b29558cde184016080d0ad976d8a4f59692120
Révai Erik <revaierik100@gmail.com>
2025-01-28 11:19:35 +0100

    Add project files.

---
+++ Kolcsonzes.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KajakKenuWPF
+{
+    internal class Kolcsonzes
+    {
+        public string Nev { get; set; }
+        public int Hajoid { get; set; }
+        public string Hajotipus { get; set; }
+        public int Szemelyekszama { get; set; }
+        public int Elvitelora { get; set; }
+        public int Elvitelperc { get; set; }
+        public int Visszahozatalora { get; set; }
+        public int Visszahozatalperc { get; set; }
+
+
+        public bool VizenVan(string ido)
+        {
+            int ora = int.Parse(ido.Split(':')[0]);
+            int perc = int.Parse(ido.Split(':')[1]);
+            if (Elvitelora == ora && Elvitelperc < perc && (Visszahozatalora > ora || Visszahozatalora == ora && Visszahozatalperc >= perc)) return true;
+            else return false;
+        }
+
+        public int KolcsonzesIdotartam()
+        {
+            return (((Visszahozatalora - Elvitelora) * 60) + (Visszahozatalperc - Elvitelperc));
+        }
+
+        public int MegkezdettFelOrak()
+        {
+            return ((KolcsonzesIdotartam() % 30 != 0) ? KolcsonzesIdotartam() / 30 + 1 : KolcsonzesIdotartam() / 30);
+        }
+
+        public override string ToString()
+        {
+            return $"{Hajoid} - {KolcsonzesIdotartam()} - {MegkezdettFelOrak()}";
+        }
+
+        public Kolcsonzes(string row)
+        {
+            string[] data = row.Split(';');
+            Nev = data[0];
+            Hajoid = int.Parse(data[1]);
+            Hajotipus = data[2];
+            Szemelyekszama = int.Parse(data[3]);
+            Elvitelora = int.Parse(data[4]);
+            Elvitelperc = int.Parse(data[5]);
+            Visszahozatalora = int.Parse(data[6]);
+            Visszahozatalperc = int.Parse(data[7]);
+        }
+
+
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows;
+using KajakKenuWPF;
+
+namespace KajakKenuWPF
+{
+    public partial class MainWindow : Window
+    {
+        List<Kolcsonzes> kolcsonzesek = [];
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+            using (StreamReader be = new(path: @"..\..\..\src\kolcsonzes.txt", encoding: Encoding.UTF8))
+            {
+                _ = be.ReadLine();
+                while (!be.EndOfStream)
+                {
+                    Kolcsonzes kolcsonzes = new(be.ReadLine());
+                    kolcsonzesek.Add(kolcsonzes);
+                }
+            }
+            Kolcsonzesek_Grid.ItemsSource = kolcsonzesek;
+        }
+
+        
+
+        private void Kereses_Click(object sender, RoutedEventArgs e)
+        {
+           
+        }
+    }
+}
