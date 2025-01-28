2555fbe89d5fe7c5d4ba773cf2ec7efef4e9cc24
Révai Erik <revaierik100@gmail.com>
2025-01-28 14:01:20 +0100

    sds

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -11,7 +11,7 @@ namespace KajakKenuWPF
         public MainWindow()
         {
             InitializeComponent();
-
+            Kolcsonzesek_Listbox = null;
 
             using (StreamReader be = new(path: @"..\..\..\src\kolcsonzes.txt", encoding: Encoding.UTF8))
             {
@@ -22,14 +22,34 @@ namespace KajakKenuWPF
                     kolcsonzesek.Add(kolcsonzes);
                 }
             }
+ 
             Kolcsonzesek_Grid.ItemsSource = kolcsonzesek;
+
+            for (int i = 0; i < 24; i++) Ora.Items.Add(i);
+            for (int i = 0; i < 60; i++) Perc.Items.Add(i);
+
         }
 
-        
+       
+        private void OsszesKolcsonzes_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (Kolcsonzes kolcsonzes in kolcsonzesek)
+            {
+                Kolcsonzesek_Listbox.Items.Add(kolcsonzes.ToString());
+            }
+        }
+
+
+
 
         private void Kereses_Click(object sender, RoutedEventArgs e)
         {
-           
+            KeresesEredmeny.Items.Clear();
+          
+            string ido = $"{Ora.Text}:{Perc.Text}";
+            var eredmeny = kolcsonzesek.FindAll(k => k.VizenVan(ido)).ToList();
+            foreach (var item in eredmeny) KeresesEredmeny.Items.Add(item);
+            
         }
     }
 }
